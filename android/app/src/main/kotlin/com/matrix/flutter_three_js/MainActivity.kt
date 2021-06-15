7e97045bf43edbb44bdf18fa35b4f3d8e90d1fc9
it5prasoon <prasoonk187@gmail.com>
2021-06-15 10:16:50 +0530

    :rocket: initial commit

---
+++ android/app/src/main/kotlin/com/matrix/flutter_three_js/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.matrix.flutter_three_js
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:jaguar/jaguar.dart';
+import 'package:jaguar_flutter_asset/jaguar_flutter_asset.dart';
+
+void main() async {
+  final server = Jaguar();
+  server.addRoute(serveFlutterAssets());
+  await server.serve(logRequests: true);
+
+  // debug
+  server.log.onRecord.listen((r) => print(r));
+
+  runApp(MaterialApp(home: WebViewExample()));
+}
+
+class WebViewExample extends StatelessWidget {
+  final Completer<WebViewController> _controller =
+  Completer<WebViewController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter WebView example'),
+      ),
+      body: Builder(builder: (BuildContext context) {
+        return WebView(
+          initialUrl: 'http://localhost:8080/suzanne.html',
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller.complete(webViewController);
+          },
+        );
+      }),
+      floatingActionButton: favoriteButton(),
+    );
+  }
+
+  Widget favoriteButton() {
+    return FutureBuilder<WebViewController>(
+        future: _controller.future,
+        builder: (BuildContext context,
+            AsyncSnapshot<WebViewController> controller) {
+          if (controller.hasData) {
+            return FloatingActionButton(
+              onPressed: () async {
+                controller.data.evaluateJavascript(
+                    "suzanne.scene.children[0].material.color.setHex('0x' + Math.floor(Math.random() * 16777215).toString(16));");
+              },
+              child: const Icon(Icons.refresh),
+            );
+          }
+          return Container();
+        });
+  }
+}
